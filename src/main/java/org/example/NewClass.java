db0a3ffc121c9063e845b3937dc657263da0b06e
Vishal <vikytyagi1800@gmail.com>
2024-10-05 19:41:40 +0530

    Second commit

--- src/main/java/org/example/NewClass.java
+++ src/main/java/org/example/NewClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("I am still trying my changes");
     }
 }
